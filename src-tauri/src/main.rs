2b78c7a7beee480fa02c5e66e4fd1358cc453613
sansan <kuangsan.li@tuya.com>
2023-06-06 15:41:42 +0800

    feat: 监听快捷键获取粘贴板

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,6 @@
 use std::{fs::File, io::Read};
 use tauri::{
+    async_runtime::spawn,
     generate_context,
     ActivationPolicy::Accessory,
     Manager, RunEvent,
@@ -7,14 +8,34 @@ use tauri::{
     WindowBuilder, WindowEvent, WindowUrl,
 };
 mod menu;
+use arboard::Clipboard;
+use livesplit_hotkey::{Hook, Hotkey, KeyCode, Modifiers};
+use tokio::time::{sleep, Duration};
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_clipboard() -> String {
+    let mut clipboard = Clipboard::new().unwrap();
+    println!("Clipboard text was: {}", clipboard.get_text().unwrap());
+    return clipboard.get_text().unwrap();
 }
 
 fn main() {
+    let hook = Hook::new().unwrap();
+    hook.register(
+        Hotkey {
+            key_code: KeyCode::KeyC,
+            modifiers: Modifiers::META,
+        },
+        || {
+            spawn(async {
+                sleep(Duration::from_secs(1)).await;
+                let mut clipboard = Clipboard::new().unwrap();
+                println!("Clipboard text was: {}", clipboard.get_text().unwrap());
+            });
+        },
+    )
+    .unwrap();
+
     tauri::Builder::default()
         .setup(|app| {
             WindowBuilder::new(app, "main", WindowUrl::App("http://localhost:3333/".into()))
@@ -29,19 +50,25 @@ fn main() {
                 .unwrap();
 
             app.set_activation_policy(Accessory);
+            // app.get_window("main").unwrap().open_devtools();
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_clipboard])
         .system_tray(menu::get_tray_menu())
         .on_system_tray_event(|app, event| match event {
             LeftClick { position, .. } => {
                 // 定位用
                 let window = app.get_window("main").unwrap();
+                window.hide().unwrap();
                 window.set_position(position).unwrap();
             }
             MenuItemClick { id, .. } => match id.as_str() {
                 "quit" => {
                     std::process::exit(0);
+                }
+                "time" => {
+                    let mut clipboard = Clipboard::new().unwrap();
+                    println!("Clipboard text was: {}", clipboard.get_text().unwrap());
                 }
                 "copy" => {
                     let window = app.get_window("main").unwrap();
@@ -55,8 +82,6 @@ fn main() {
         })
         .on_window_event(|event| match event.event() {
             WindowEvent::CloseRequested { api, .. } => {
-                println!("{:#?}", 1111111);
-                
                 event.window().hide().unwrap();
                 api.prevent_close();
             }
@@ -71,7 +96,6 @@ fn main() {
         .expect("error while running tauri application")
         .run(|_app_handle, event| match event {
             RunEvent::ExitRequested { api, .. } => {
-                println!("{:#?}", 222222);
                 api.prevent_exit();
             }
             _ => {}
